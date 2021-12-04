8ab62da8bf53bda9195f665e94aa7a74a5e80cd7
zacharytse <2481086105@qq.com>
2021-12-04 18:49:38 +0800

    Delete gfs-common directory

--- gfs-common/src/main/java/com/xcq/common/remote/IHeartBeat.java
+++
@@ -1,9 +0,0 @@
-package com.xcq.common.remote;
-
-import com.xcq.common.dto.ServerInfo;
-
-public interface IHeartBeat {
-    void addServer(ServerInfo serverInfo);
-
-    void deleteServer(ServerInfo serverInfo);
-}
--- gfs-common/src/main/java/com/xcq/common/remote/IRpc.java
+++
@@ -1,8 +0,0 @@
-package com.xcq.common.remote;
-
-import com.xcq.common.rpc.Request;
-import com.xcq.common.rpc.Response;
-
-public interface IRpc {
-    Response send(String serverName, Request request);
-}
